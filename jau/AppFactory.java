19b596213b4c5690200af98a3f58cd9887bc761c
baumannn <alxbaumann@gmail.com>
2014-05-12 15:50:23 -0500

    fact

--- jau/AppFactory.java
+++ jau/AppFactory.java
@@ -1,10 +1,48 @@
-public class AppFactory {
-  public static void main(String args[]) {
-    ConnectionFactory factory;
-    factory = new ConnectionFactory("suma");
-    Connection connection = factory.createConnection();
-    System.out.println("Estas conectado a " + connection.description());
-    System.out.println("Resultado de la operacion: " + connection.operacion(8, 2));
+abstract class Connection  {
+  public Connection() { }
+  public String description() {
+    return "Generic";
+  }
+  public abstract int operation (int a, int b);
+}
+
+class SumaConnection extends Connection {
+  public SumaConnection() { }
+  public String description() {
+    return "suma";
+  }
+  public int operation (int a, int b) {
+	return a + b;
+  }
+}
+
+class RestaConnection extends Connection {
+  public RestaConnection() { }
+  public String description() {
+    return "resta";
+  }
+   public int operation (int a, int b) {
+	return a - b;
+  }
+}
+
+class MultiplicacionConnection extends Connection {
+  public MultiplicacionConnection() { }
+  public String description() {
+    return "multiplicacion";
+  }
+   public int operation (int a, int b) {
+	return a * b;
+  }
+}
+
+class DivisionConnection extends Connection {
+  public DivisionConnection() { }
+  public String description() {
+    return "division";
+  }
+   public int operation (int a, int b) {
+	return a / b;
   }
 }
 
@@ -24,60 +62,20 @@ class ConnectionFactory {
     else if (type.equals("multiplicacion")){
       return new MultiplicacionConnection();
     }
-	else if (type.equals("division")){
+  else if (type.equals("division")){
       return new DivisionConnection();
     }
-	
+  
     else return null;
   }
 }
 
-abstract class Connection  {
-  public Connection() { }
-  public String description() {
-    return "Generic";
-  }
-  public abstract int operacion (int a, int b);
-}
-
-class SumaConnection extends Connection {
-  public SumaConnection() { }
-  public String description() {
-    return "suma";
-  }
-  public int operacion (int a, int b) {
-	return a + b;
+public class AppFactory {
+  public static void main(String args[]) {
+    ConnectionFactory factory;
+    factory = new ConnectionFactory("suma");
+    Connection connection = factory.createConnection();
+    System.out.println("Estas conectado a " + connection.description());
+    System.out.println("Resultado de la operacion: " + connection.operation(8, 2));
   }
 }
-
-class RestaConnection extends Connection {
-  public RestaConnection() { }
-  public String description() {
-    return "resta";
-  }
-   public int operacion (int a, int b) {
-	return a - b;
-  }
-}
-
-class MultiplicacionConnection extends Connection {
-  public MultiplicacionConnection() { }
-  public String description() {
-    return "multiplicacion";
-  }
-   public int operacion (int a, int b) {
-	return a * b;
-  }
-}
-
-class DivisionConnection extends Connection {
-  public DivisionConnection() { }
-  public String description() {
-    return "division";
-  }
-   public int operacion (int a, int b) {
-	return a / b;
-  }
-}
-
-
