622cd37178d242a8c0310f8e3b52db7bc81429f5
baumannn <alxbaumann@gmail.com>
2014-05-12 15:01:26 -0500

    temp

--- (adapter-fachada)/AppAdapter.java
+++ (adapter-fachada)/AppAdapter.java
@@ -77,13 +77,75 @@ public class AppAdapter { //This is the modified client
   }
 }
 
+
+interface Celsius {
+  public void setTemperatura(int t);
+  public int getTemperatura();
+}
+
+class Temperatura implements Celcius{
+  int temp;
+
+  public int getTemperatura(){
+    return temp;
+  }
+  public void setTemperatura(int t){
+    temp = t;
+  }
+
+}
+
+interface Fahrenheit {
+  public void setTemperature(int t);
+  public int getTemperature();
+}
+
+class Temperature implements Fahrenheit{
+  int temp;
+
+  public int getTemperature(){
+    return temp;
+  }
+
+  public void setTemperature(int t){
+    temp = t;
+  }
+}
+
+
+class TempAdapter implements Celsius {
+  
+  Temperature fahrenObject;
+
+  TempAdapter(){
+    fahrenObject = new Temperature();
+  }
+
+  public void setTemperatura(int t) {
+    fahrenObject.temp = t*9/5 + 32;
+  }
+
+  public int getTemperatura() {
+    return (fahrenObject.temp - 32) * 5/9;
+  }
+
+}
+
+public class TemperatureAdapter {
+  public static void main(String args[]) {
+    TempAdapter celciusObject = new TempAdapter();
+    celciusObject.setTemperatura(15);
+    System.out.println("Temperatura: " + celciusObject.getTemperatura());
+  }
+}
+
 // EJERCICIO: Elaborar una aplicacion que utilice el patron Adapter
 
 // Existe una clase Temperatura que opera con temperaturas en grados Celsius, con
 // metodos para getTemperatura y setTemperatura.
 
 // La nueva clase Temperature opera con temperaturas en grados Fahrenheit, con
-// metodos setTemperature y getTempreature.
+// metodos setTemperature y getTemperature.
 
 // El cliente siempre espera guardar y recibir temperaturas en grados Celsius.
 
